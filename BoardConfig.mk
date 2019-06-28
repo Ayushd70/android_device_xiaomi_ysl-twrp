df514bf55e06b9397b8930373bedce69dc93de64
Ayush Dubey <ayushdubey70@gmail.com>
2019-06-28 20:32:58 +0530

    ysl: twrp toolbox
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,7 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/fstab.qcom
 BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_USE_TOOLBOX := true
 
 # Disable Mouse Cursor
 TW_INPUT_BLACKLIST := "hbtp_vm"
